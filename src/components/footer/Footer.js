2e6938b1203e6af027dc4ce5e4662142fc257721
Dennis Sweeris <dennissweeris@Hotmail.com>
2021-05-04 23:29:10 +0200

    Update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,10 +3,18 @@ import React from "react";
 function Footer() {
   return (
     <footer>
+      <div class="footer-info">
+        <p>Copyright © 2021 Dennis Sweeris</p>
+      </div>
       <ul>
         <li>
+          <a href="https://linkedin.com/DennisSweeris" target="_blank" rel="noreferrer">
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </li>
+        <li>
           <a href="https://github.com/DennisSweeris" target="_blank" rel="noreferrer">
-            <i className="fab fa-github"></i>
+            <i class="fab fa-github"></i>
           </a>
         </li>
       </ul>
